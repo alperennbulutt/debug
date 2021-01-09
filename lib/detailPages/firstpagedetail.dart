91f9aabc654244236a2e507695d0705ecd8a2089
alperennbulutt <alperenbulut.ab@gmail.com>
2021-01-09 17:54:14 +0300

    detaysayfası ve firstpage veritabanından veriler alındı

--- lib/detailPages/firstpagedetail.dart
+++ lib/detailPages/firstpagedetail.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animations/loading_animations.dart';
 
 class FirstPageDetail extends StatefulWidget {
+  final int index;
+  FirstPageDetail({this.index});
   @override
   _FirstPageDetailState createState() => _FirstPageDetailState();
 }
@@ -9,42 +13,66 @@ class FirstPageDetail extends StatefulWidget {
 class _FirstPageDetailState extends State<FirstPageDetail> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: Center(child: Text("Haber baslik burada yazacak")),
-        ),
-        body: Row(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: SizedBox(
-                child: Column(
-                  children: [
-                    Image.asset("assets/images/logo3.png"),
-                    Text(
-                      "haber icerik alakalı şeyeler burada yazacaktır lutfen duzenlı bir sekilde okuyunuz",
-                      style: TextStyle(color: Colors.black, fontSize: 15),
-                    ),
-                  ],
+    return StreamBuilder<QuerySnapshot>(
+      stream:
+          FirebaseFirestore.instance.collection('firstpagedetail').snapshots(),
+      builder: (context, snapshot) {
+        return !snapshot.hasData
+            //loading page is here
+            ? Scaffold(
+                backgroundColor: Colors.white,
+                body: Container(
+                  child: LoadingFlipping.circle(
+                    borderColor: Colors.white,
+                    borderSize: 3.0,
+                    size: 75.0,
+                    backgroundColor: Colors.white,
+                    duration: Duration(milliseconds: 500),
+                  ),
                 ),
-              ),
-            ),
-            Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              children: [
-                Text(
-                  "haber tarihi",
-                  style: TextStyle(color: Colors.black38),
+              )
+            : MaterialApp(
+                debugShowCheckedModeBanner: false,
+                home: Scaffold(
+                  appBar: AppBar(
+                    backgroundColor: Colors.amber,
+                    title: Center(
+                        child:
+                            Text(snapshot.data.docs[widget.index]["baslik"])),
+                  ),
+                  body: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          child: Column(
+                            children: [
+                              Image.network(
+                                  snapshot.data.docs[widget.index]["foto"]),
+                              Text(
+                                snapshot.data.docs[widget.index]["icerik"],
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 15),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          Text(
+                            snapshot.data.docs[widget.index]["tarih"],
+                            style: TextStyle(color: Colors.black38),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ],
-            )
-          ],
-        ),
-      ),
+              );
+      },
     );
   }
 }
--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -1,126 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:debug/detailPages/firstpagedetail.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animations/loading_animations.dart';
 
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, //debug yazısını kaldırmak icin
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  //1.kısım yatay kaydırılan
-                  Container(
-                    height: 300,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => FirstPageDetail()),
-                            );
-                          },
-                          child: Container(
-                            width: 300,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        Container(
-                          width: 300,
-                          color: Colors.blueGrey,
-                        ),
-                        Container(
-                          width: 300,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: 300,
-                          color: Colors.grey,
-                        )
-                      ],
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("firtspage").snapshots(),
+        builder: (context, snapshot) {
+          return !snapshot.hasData
+              ? Scaffold(
+                  backgroundColor: Colors.amber,
+                  //loading page is here
+                  body: Container(
+                    child: LoadingFlipping.circle(
+                      borderColor: Colors.white,
+                      borderSize: 3.0,
+                      size: 75.0,
+                      backgroundColor: Colors.orange,
+                      duration: Duration(milliseconds: 500),
                     ),
                   ),
-                  //2.kısım dikey kayan yer
-                  Container(
-                    height: 300,
-                    child: ListView(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.vertical,
-                      children: <Widget>[
-                        Container(
-                          width: 50,
-                          height: 100,
-                          color: Colors.yellowAccent,
-                        ),
-                        Container(
-                          width: 50,
-                          height: 100,
-                          color: Colors.blue,
-                        ),
-                        Container(
-                          width: 50,
-                          height: 100,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: 50,
-                          height: 100,
-                          color: Colors.red,
-                        ),
-                        Container(
-                          width: 50,
-                          height: 100,
-                          color: Colors.yellowAccent,
-                        ),
-                        Container(
-                          width: 50,
-                          height: 100,
-                          color: Colors.blue,
-                        ),
-                        Container(
-                          width: 50,
-                          height: 100,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: 50,
-                          height: 100,
-                          color: Colors.red,
-                        )
-                      ],
-                    ),
-                  ),
-                  //3.kısım yatay kayan yer
-                  Container(
-                    height: 500,
-                    child: GridView.count(
-                      scrollDirection: Axis.horizontal,
-                      crossAxisCount: 2,
-                      children: List.generate(8, (index) {
-                        return Container(
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(32),
+                )
+              : GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1),
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot products = snapshot.data.docs[index];
+                    {
+                      return MaterialApp(
+                        debugShowCheckedModeBanner:
+                            false, //debug yazısını kaldırmak icin
+                        home: Scaffold(
+                          backgroundColor: Colors.amber,
+                          body: SafeArea(
+                            child: SingleChildScrollView(
+                              child: Container(
+                                child: Column(
+                                  children: <Widget>[
+                                    //1.kısım yatay kaydırılan
+                                    Container(
+                                      height: 300,
+                                      child: ListView(
+                                        scrollDirection: Axis.horizontal,
+                                        children: <Widget>[
+                                          GestureDetector(
+                                            onTap: () {
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        FirstPageDetail(
+                                                            index: index)),
+                                              );
+                                            },
+                                            child: Container(
+                                              width: 300,
+                                              child: Scaffold(
+                                                appBar: AppBar(
+                                                  title:
+                                                      Text(products['baslik']),
+                                                ),
+                                                body: Column(
+                                                  children: [
+                                                    Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        Text(products['tarih']),
+                                                      ],
+                                                    ),
+                                                    Image.network(
+                                                        products['foto']),
+                                                    Text(products['icerik']),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
-                            color: Colors.black,
                           ),
-                        );
-                      }),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+                        ),
+                      );
+                    }
+                  },
+                );
+        });
   }
 }

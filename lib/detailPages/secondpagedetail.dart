7aaf50ac78425db688f95e4f018fdb9f3936db23
alperennbulutt <alperenbulut.ab@gmail.com>
2021-01-12 02:14:48 +0300

    büyük ölçüde tamam

---
+++ lib/detailPages/secondpagedetail.dart
@@ -0,0 +1,108 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:loading_animations/loading_animations.dart';
+
+class SecondPageDetail extends StatefulWidget {
+  final int index;
+  SecondPageDetail({this.index});
+  @override
+  _SecondPageDetailState createState() => _SecondPageDetailState();
+}
+
+class _SecondPageDetailState extends State<SecondPageDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30)),
+        ),
+        backgroundColor: Colors.amber[100],
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("secondpagedetail")
+              .snapshots(),
+          builder: (context, snapshot) {
+            //print("deneme 1234 " + widget.index.toString());
+            return !snapshot.hasData
+                //loading page is here
+                ? Scaffold(
+                    backgroundColor: Colors.white,
+                    body: Container(
+                      child: LoadingFlipping.circle(
+                        borderColor: Colors.white,
+                        borderSize: 3.0,
+                        size: 75.0,
+                        backgroundColor: Colors.white,
+                        duration: Duration(milliseconds: 500),
+                      ),
+                    ),
+                  )
+                : SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  snapshot.data.docs[widget.index]["baslik"],
+                                  style: TextStyle(
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                            child: Image.network(
+                              snapshot.data.docs[widget.index]["foto"],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(1.0),
+                              child: Text(
+                                snapshot.data.docs[widget.index]["tarih"],
+                                style: TextStyle(
+                                  color: Color(0xFF325384).withOpacity(0.7),
+                                  fontSize: 12.0,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            snapshot.data.docs[widget.index]["icerik"],
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -1,97 +1,117 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animations/loading_animations.dart';
+
+import 'detailPages/secondpagedetail.dart';
 
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      //debug yazısını silmek için
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        body: SafeArea(
-          //1.kısım dikey kaydırılan alan
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: 200,
-                  child: ListView(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.vertical,
-                    children: <Widget>[
-                      Container(
-                        width: 50,
-                        height: 100,
-                        color: Colors.yellowAccent,
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("secondpage").snapshots(),
+      builder: (context, snapshot) {
+        return !snapshot.hasData
+            ? Scaffold(
+                backgroundColor: Colors.amber,
+                //loading page is here
+                body: Container(
+                  child: LoadingFlipping.circle(
+                    borderColor: Colors.white,
+                    borderSize: 3.0,
+                    size: 75.0,
+                    duration: Duration(milliseconds: 500),
+                  ),
+                ),
+              )
+            : Scaffold(
+                //yatay kısım
+                body: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 200,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            itemCount:
+                                2, //veritanabından ilk iki haber burada gösterilecek
+                            itemBuilder: (context, index) {
+                              DocumentSnapshot products =
+                                  snapshot.data.docs[index];
+
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            SecondPageDetail(index: index)),
+                                  );
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    width: 100,
+                                    color: Colors.green,
+                                    child: Column(
+                                      children: [
+                                        Text(products['baslik']),
+                                        Image.network(products['foto']),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
                       ),
                       Container(
-                        width: 50,
-                        height: 100,
-                        color: Colors.blue,
+                        height: 300,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount:
+                              3, //veritanabından yukarıda gösterilen 2 haberden sonra 3 haber de burada gösterilecek
+                          itemBuilder: (context, index) {
+                            DocumentSnapshot products = snapshot.data.docs[
+                                index +
+                                    2]; //yukarıda gösterilmiş iki haber atlandı
+
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          SecondPageDetail(index: index + 2)),
+                                );
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  width: 200,
+                                  color: Colors.green,
+                                  child: Column(
+                                    children: [
+                                      Text(products["baslik"]),
+                                      Image.network(products['foto']),
+                                      Text(products["icerik"]),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
                       ),
-                      Container(
-                        width: 50,
-                        height: 100,
-                        color: Colors.green,
-                      ),
-                      Container(
-                        width: 50,
-                        height: 100,
-                        color: Colors.red,
-                      ),
-                      Container(
-                        width: 50,
-                        height: 100,
-                        color: Colors.yellowAccent,
-                      ),
-                      Container(
-                        width: 50,
-                        height: 100,
-                        color: Colors.blue,
-                      ),
-                      Container(
-                        width: 50,
-                        height: 100,
-                        color: Colors.green,
-                      ),
-                      Container(
-                        width: 50,
-                        height: 100,
-                        color: Colors.red,
-                      )
                     ],
                   ),
                 ),
-                //2.kısım yatay kaydırılan
-                Container(
-                  height: 300,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      Container(
-                        width: 300,
-                        color: Colors.grey,
-                      ),
-                      Container(
-                        width: 300,
-                        color: Colors.blueGrey,
-                      ),
-                      Container(
-                        width: 300,
-                        color: Colors.green,
-                      ),
-                      Container(
-                        width: 300,
-                        color: Colors.grey,
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+              );
+      },
     );
   }
 }
